09d66fa7bd304254b9f7c696b7f5c82405b64d93
TomHSES <94571187+TomHSES@users.noreply.github.com>
2024-03-10 10:29:30 -0400

    Run Talon at CAN ID 5 when X pressed, add autoformatting configuration and format all files

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,11 +4,6 @@
 
 package frc.robot;
 
-import com.ctre.phoenix6.StatusCode;
-import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.controls.Follower;
-import com.ctre.phoenix6.controls.NeutralOut;
-import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 import edu.wpi.first.wpilibj.TimedRobot;
@@ -27,18 +22,12 @@ public class Robot extends TimedRobot {
 
   private RobotContainer m_robotContainer;
 
+  // motors
   private static final String canBusName = "";
   private final TalonFX m_fx = new TalonFX(5, canBusName);
-  // private final TalonFX m_fllr = new TalonFX(6, canBusName);
-  
-  /* Be able to switch which control request to use based on a button press */
-  /* Start at velocity 0, enable FOC, no feed forward, use slot 0 */
-  private final VelocityVoltage m_voltageVelocity = new VelocityVoltage(0, 0, true, 0, 0, false, false, false);
-  /* Keep a neutral out so we can disable the motor */
-  private final NeutralOut m_brake = new NeutralOut();
 
+  // controllers
   private final Joystick m_joystick = new Joystick(0);
-
 
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -46,56 +35,36 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
-    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
+    // Instantiate our RobotContainer. This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
-
-    TalonFXConfiguration configs = new TalonFXConfiguration();
-
-    /* Voltage-based velocity requires a feed forward to account for the back-emf of the motor */
-    configs.Slot0.kP = 0.11; // An error of 1 rotation per second results in 2V output
-    configs.Slot0.kI = 0.5; // An error of 1 rotation per second increases output by 0.5V every second
-    configs.Slot0.kD = 0.0001; // A change of 1 rotation per second squared results in 0.01 volts output
-    configs.Slot0.kV = 0.12; // Falcon 500 is a 500kV motor, 500rpm per V = 8.333 rps per V, 1/8.33 = 0.12 volts / Rotation per second
-    // Peak output of 8 volts
-    configs.Voltage.PeakForwardVoltage = 8;
-    configs.Voltage.PeakReverseVoltage = -8;
-
-    /* Retry config apply up to 5 times, report if failure */
-    StatusCode status = StatusCode.StatusCodeNotInitialized;
-    for (int i = 0; i < 5; ++i) {
-      status = m_fx.getConfigurator().apply(configs);
-      if (status.isOK()) break;
-    }
-    if(!status.isOK()) {
-      System.out.println("Could not apply configs, error code: " + status.toString());
-    }
-
-    // m_fllr.setControl(new Follower(m_fx.getDeviceID(), false));
   }
 
   /**
    * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
    * that you want ran during disabled, autonomous, teleoperated and test.
    *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+   * <p>
+   * This runs after the mode specific periodic functions, but before LiveWindow and
    * SmartDashboard integrated updating.
    */
   @Override
   public void robotPeriodic() {
-    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
+    // Runs the Scheduler. This is responsible for polling buttons, adding newly-scheduled
     // commands, running already-scheduled commands, removing finished or interrupted commands,
-    // and running subsystem periodic() methods.  This must be called from the robot's periodic
+    // and running subsystem periodic() methods. This must be called from the robot's periodic
     // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
   }
 
   /** This function is called once each time the robot enters Disabled mode. */
   @Override
-  public void disabledInit() {}
+  public void disabledInit() {
+  }
 
   @Override
-  public void disabledPeriodic() {}
+  public void disabledPeriodic() {
+  }
 
   /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override
@@ -117,7 +86,8 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically during autonomous. */
   @Override
-  public void autonomousPeriodic() {}
+  public void autonomousPeriodic() {
+  }
 
   @Override
   public void teleopInit() {
@@ -133,14 +103,13 @@ public class Robot extends TimedRobot {
   /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
-    boolean buttonPressed = m_joystick.getRawButton(0);
+    boolean buttonPressed = m_joystick.getRawButton(1);
     if (buttonPressed) {
       /* Use voltage velocity */
-      m_fx.setControl(m_voltageVelocity.withVelocity(30));
-    }    
-    else {
+      m_fx.set(.1);
+    } else {
       /* Disable the motor instead */
-      m_fx.setControl(m_brake);
+      m_fx.set(0);
     }
   }
 
@@ -152,5 +121,6 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically during test mode. */
   @Override
-  public void testPeriodic() {}
+  public void testPeriodic() {
+  }
 }
