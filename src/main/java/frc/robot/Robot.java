8f3be987767f12cd2703cc46b51a8c3ae17e2984
TomHSES <94571187+TomHSES@users.noreply.github.com>
2024-03-29 20:14:17 -0400

    extra files

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,7 +5,9 @@
 package frc.robot;
 
 
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
@@ -20,6 +22,7 @@ public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
+  private DutyCycleEncoder armEncoder;
   
   // motors
   // private static final String canBusName = "";
@@ -37,6 +40,8 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer. This will perform all our button bindings,
     // and put our autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+    armEncoder = new DutyCycleEncoder(8);
+        armEncoder.reset();
   }
 
   /**
@@ -56,11 +61,13 @@ public class Robot extends TimedRobot {
     // This must be called from the robot's periodic block in order for anything in
     // the Command-based framework to work.
     CommandScheduler.getInstance().run();
+    SmartDashboard.putNumber("armEncoder", armEncoder.getPositionOffset());
   }
 
   /** This function is called once each time the robot enters Disabled mode. */
   @Override
   public void disabledInit() {
+    armEncoder.reset();
   }
 
   @Override
@@ -95,6 +102,7 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopInit() {
+    armEncoder.reset();
     // This makes sure that the autonomous stops running when
     // teleop starts running. If you want the autonomous to
     // continue until interrupted by another command, remove
--- src/main/java/frc/robot/subsystems/AdvancedArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/AdvancedArmSubsystem.java
@@ -1,27 +1,49 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.configs.FeedbackConfigs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.controls.compound.Diff_PositionVoltage_Velocity;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
+import com.ctre.phoenix6.signals.GravityTypeValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class AdvancedArmSubsystem extends SubsystemBase {
-    private final TalonFX left_motor = new TalonFX(12, "");
-    private final TalonFX right_motor = new TalonFX(11, "");
+    private final TalonFX left_motor = new TalonFX(12);
+    private final TalonFX right_motor = new TalonFX(11);
 
     private static double move_up_pct_power = 0.4;
     private static double move_down_pct_power = -0.4;
     private static double hold_position_pct_power = 0;
 
+    private DutyCycleEncoder armEncoder;
+
+    PositionVoltage positionControl;
     public AdvancedArmSubsystem() {
         super();
+        TalonFXConfiguration config = new TalonFXConfiguration();
+        config.Slot0.kP = 1;
+        config.Slot0.GravityType = GravityTypeValue.Arm_Cosine;
         left_motor.setInverted(false);
         right_motor.setInverted(true);
 
+        Follower follower = new Follower(left_motor.getDeviceID(), true);
+        right_motor.setControl(follower);
+        right_motor.getConfigurator().apply(config);
         left_motor.setNeutralMode(NeutralModeValue.Brake);
         right_motor.setNeutralMode(NeutralModeValue.Brake);
+
+        //TODO: Change this to 8. It causes conflicts until the Robot.java encoder is removed.
+        armEncoder = new DutyCycleEncoder(7);
+
+        positionControl = new PositionVoltage(0.0);
     }
 
     public Command moveArmUpCommand() {
@@ -32,7 +54,7 @@ public class AdvancedArmSubsystem extends SubsystemBase {
                 },
                 () -> {
                     left_motor.set(hold_position_pct_power);
-                    right_motor.set(hold_position_pct_power);
+                    //right_motor.set(hold_position_pct_power);
                 }, this);
     }
 
@@ -40,26 +62,42 @@ public class AdvancedArmSubsystem extends SubsystemBase {
         return Commands.startEnd(
                 () -> {
                     left_motor.set(move_down_pct_power);
-                    right_motor.set(move_down_pct_power);
+                    //right_motor.set(move_down_pct_power);
                 },
                 () -> {
                     left_motor.set(hold_position_pct_power);
-                    right_motor.set(hold_position_pct_power);
+                    //right_motor.set(hold_position_pct_power);
                 }, this);
     }
 
     public Command setBrakeModeCommand() {
         return this.runOnce(() -> {
             left_motor.setNeutralMode(NeutralModeValue.Brake);
-            right_motor.setNeutralMode(NeutralModeValue.Brake);
+            //right_motor.setNeutralMode(NeutralModeValue.Brake);
         });
     }
 
     public Command setCoastModeCommand() {
         return this.runOnce(() -> {
             left_motor.setNeutralMode(NeutralModeValue.Coast);
-            right_motor.setNeutralMode(NeutralModeValue.Coast);
+            //right_motor.setNeutralMode(NeutralModeValue.Coast);
         });
     }
 
+    public double getEncoderReading() {
+        return armEncoder.getPositionOffset();
+    }
+
+    public void resetEncoder() {
+        armEncoder.reset();
+    }
+
+    public void moveArm(double pct_power) {
+        left_motor.set(pct_power);
+        //right_motor.set(pct_power);
+    } 
+
+    public void setPosition(double position) {
+        left_motor.setControl(positionControl.withPosition(5.0));
+    }
 }
